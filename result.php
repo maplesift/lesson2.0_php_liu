582b66adee358337cf3fca2252354a5837151996
maplesift <a23050716@gmail.com>
2024-10-28 11:56:45 +0800

    增加老師寫

--- result.php
+++ result.php
@@ -10,33 +10,33 @@
     <h1>bmi結果</h1>
     <div>您的身高:<?=$_GET['height'];?>公分</div>
     <div>您的體重:<?=$_GET['weight'];?>公斤</div>
+
     <?php
     $h=$_GET['height'] /100;
 
     $bmi=round($_GET['weight']/($h * $h),2);
-    ?>
-    <div>你的bmi為:<?=$bmi?></div>
-    <div>體位判定為:<?php
-if ($bmi<18.5) {
-    echo '過輕';
-    }elseif ($bmi<24) {
-        echo '正常範圍';
-    }elseif ($bmi<27) {
-        echo '過重';
-    }elseif ($bmi<30) {
-        echo '輕度肥胖' ;
-    }elseif ($bmi<35) {
-        echo '中度肥胖' ;
-    }elseif ($bmi>=35) {
-        echo '重度肥胖';
-    }
 
-?></div>
+    if ($bmi<18.5) {
+            $level= "過輕";
+        }elseif ($bmi<24) {
+            $level="正常範圍";
+        }elseif ($bmi<27) {
+            $level="過重";
+        }elseif ($bmi<30) {
+            $level="輕度肥胖" ;
+        }elseif ($bmi<35) {
+            $level="中度肥胖" ;
+        }elseif ($bmi>=35) {
+            $level="重度肥胖";
+        }
+    ?>
+    <div>你的bmi為:<?=$bmi; ?></div>
+    <div>體位判定為:<?=$level; ?></div>
     <div>
         <a href="index.php">重新量測</a>
     </div>
     <div></div>
-    
+
     <!-- 
     體重過輕
 BMI ＜ 18.5
@@ -51,5 +51,21 @@ BMI ＜ 18.5
 輕度肥胖：27≦BMI＜30
 中度肥胖：30≦BMI＜35
 重度肥胖：BMI≧35 -->
+<!-- 自己寫
+ <?php 
+if ($bmi<18.5) {
+    echo '過輕';
+}elseif ($bmi<24) {
+        echo '正常範圍';
+    }elseif ($bmi<27) {
+        echo '過重';
+    }elseif ($bmi<30) {
+        echo '輕度肥胖' ;
+    }elseif ($bmi<35) {
+        echo '中度肥胖' ;
+    }elseif ($bmi>=35) {
+        echo '重度肥胖';
+    }
+?> -->
 </body>
 </html>
