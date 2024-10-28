2311737bfbeb206239e910a0e42c6386653b5345
maplesift <a23050716@gmail.com>
2024-10-28 11:14:21 +0800

    設計表單

--- index.php
+++ index.php
@@ -4,8 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>計算bmi</title>
+<link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <h1>計算bmi</h1>
+    <ul>
+        <li>建立一個可以輸入身高和體重的表單畫面</li>
+        <li>按下"計算BMI"按鈕後，在另一個頁面顯示BMI值</li>
+    </ul>
+    <form action="result.php" method="post">
+        <div>
+            <label for="height">身高</label>
+            <input type="number" name="height" id="height">/cm
+        </div>
+        <div>
+            <label for="weight">體重</label>
+            <input type="number" name="weight" id="weight">/kg
+        </div>
+        <div>
+            <input type="submit" value="送出">
+            <input type="reset" value="清空/重置">
+        </div>
+        <div>
+            
+        </div>
+    </form>
+
+
 </body>
 </html>
---
+++ result.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bmi結果</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1>bmi結果</h1>
+    <div>您的身高:公分</div>
+    <div>您的體重;公斤</div>
+    <div>你的bmi為:</div>
+    <div>體位判定為:</div>
+    <div>
+        <a href="index.php">重新量測</a>
+    </div>
+    <div></div>
+</body>
+</html>
