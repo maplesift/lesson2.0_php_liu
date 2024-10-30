a05386775ff19a0d3a61156f5c342e2a66145191
maplesift <a23050716@gmail.com>
2024-10-30 15:31:39 +0800

    登入檢查:使用cookie 及準備include

--- 登入檢查/check_acc.php
+++ 登入檢查/check_acc.php
@@ -1,11 +1,20 @@
-<?php
+<?php 
 
-$id=$_POST['id'];
-$pass=$_POST['pass'];
-if ($id== 'admin' && $pass=='1234') {
-    echo '登入成功';
-}else {
-    echo '登入失敗';
+if(!isset($_POST['acc'])){
+    header("location:login.php");
+    exit();
 }
 
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+if($acc=='admin' && $pw=='1234'){
+    echo "帳密正確:登入成功";
+    setcookie("login","$acc",time()+180);
+    echo $_COOKIE['login'];
+    echo "<br><a href='login.php'>回首頁</a>";
+}else{
+    echo "帳密錯誤:登入失敗";
+
+}
 ?>
